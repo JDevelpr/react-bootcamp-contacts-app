369e821e3e2f21b3280a42242dd9f579a82d452a
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-07-02 09:01:40 -0500

    initialized project, still configuring development space

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  )
+}
+
+export default App
