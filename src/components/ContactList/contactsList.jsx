1c460944c0c40edc4ea48ecd904da67b4e97c004
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-07-05 09:42:57 -0500

    successful component layout Open to changes.

---
+++ src/components/ContactList/contactsList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../assets/styles/contactsList.css";
+import Contact from "../contact/contact.jsx"
+
+function ContactsList() {
+	return (
+		<div className="contacts-list">
+			<div className="contacts-list__container-title">
+				<p className="contacts-list__tittle">Contacts List</p>
+				<div className="contacts-list__container-line">
+					<hr className="contacts-list__line" />
+				</div>
+			</div>
+			<div className="contact-list__list">
+				<Contact isAFavoriteContact={false} />
+				<Contact isAFavoriteContact={false} />
+				<Contact isAFavoriteContact={false} />
+				<Contact isAFavoriteContact={false} />
+			</div>
+		</div>
+	);
+}
+
+export default ContactsList;
