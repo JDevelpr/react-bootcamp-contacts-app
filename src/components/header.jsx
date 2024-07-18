5b7c007cca70807473fb217b4e5ccb30aeb6a011
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-07-18 11:31:19 -0500

    Update (3): 18/07/2024 CSS code improvements (@Media Queries)  Improvements to React router prompts

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../assets/styles/header.css";
 import logoGlobant from "../assets/img/globant-logo.svg";
 
 function Header() {
-  const [activeLink, setActiveLink] = useState("/overview");
-  const handleLinkClick = (path) => {
-    setActiveLink(path);
-  };
+  const location = useLocation();
+  const activeLink = location.pathname;
+
   return (
     <header className="header">
       <div className="header__container-logo">
-        <Link to="/overview" href="#" className="header__link-home" onClick={() => handleLinkClick("/overview")}>
+        <Link to="/overview" className="header__link-home">
           <img src={logoGlobant} alt="logo of Globant" className={`header__logo ${activeLink === "/overview" ? "header__link--green" : ""}`} />
         </Link>
       </div>
       <nav className="header__navigation">
         <ul className="header__list">
-          <li className="header__item" onClick={() => handleLinkClick("/overview")}>
+          <li className="header__item">
             <Link to="/overview" className={`header__link ${activeLink === "/overview" ? "header__link--green" : ""}`}>Overview</Link>
           </li>
-          <li className="header__item" onClick={() => handleLinkClick("/contact_list")}>
+          <li className="header__item">
             <Link to="/contact_list" className={`header__link ${activeLink === "/contact_list" ? "header__link--green" : ""}`}>Contacts</Link>
           </li>
-          <li className="header__item" onClick={() => handleLinkClick("/favorite_contacts")}>
+          <li className="header__item">
             <Link to="/favorite_contacts" className={`header__link ${activeLink === "/favorite_contacts" ? "header__link--green" : ""}`}>Favorites</Link>
           </li>
-          <li className="header__item" onClick={() => handleLinkClick("/new_contacts")}>
+          <li className="header__item">
             <Link to="/new_contacts" className={`header__link ${activeLink === "/new_contacts" ? "header__link--green" : ""}`}>
               <button className="header__button--green">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
@@ -43,6 +42,3 @@ function Header() {
 }
 
 export default Header;
-
-
-
