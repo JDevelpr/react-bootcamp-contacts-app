f0d33a9658d06e8a305d125ba66052d6df07cfa6
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-07-18 10:59:33 -0500

    Update: 18/07/2024 Code and user experience improvements

--- src/components/favoriteContacts.jsx
+++ src/components/favoriteContacts.jsx
@@ -1,8 +1,8 @@
-// src/components/FavoriteContacts.jsx
 import React, { useContext } from "react";
 import "../assets/styles/favoriteContacts.css";
 import Contact from "../components/contact/contact.jsx";
 import { ContactContext } from "../context/ContactContext";
+import { Link } from "react-router-dom";
 
 function FavoriteContacts() {
   const { state, dispatch } = useContext(ContactContext);
@@ -20,17 +20,30 @@ function FavoriteContacts() {
         </div>
       </div>
       <div className="favorite-contacts__list">
-        {state.favorites.map((user) => (
-          <Contact
-            key={user.id}
-            isAFavoriteContact={true}
-            email={user.email}
-            first_name={user.first_name}
-            last_name={user.last_name}
-            avatar={user.avatar}
-            onRemoveFavorite={() => handleRemoveFavorite(user.id)}
-          />
-        ))}
+        {state.favorites.length === 0 ? (
+          <div className="favorite-contacts__empty">
+            <p className="favorite-contacts__empty-message">
+              Oh! It looks like you dont have favorite contacts yet.
+            </p>
+            <Link to="/new_contacts" className="favorite-contacts__empty-add">
+              <p className="favorite-contacts__empty-add">
+                Add contacts to favorites.
+              </p>
+            </Link>
+          </div>
+        ) : (
+          state.favorites.map((user) => (
+            <Contact
+              key={user.id}
+              isAFavoriteContact={true}
+              email={user.email}
+              first_name={user.first_name}
+              last_name={user.last_name}
+              avatar={user.avatar}
+              onRemoveFavorite={() => handleRemoveFavorite(user.id)}
+            />
+          ))
+        )}
       </div>
     </div>
   );
