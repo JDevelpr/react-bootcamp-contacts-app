78e10b9df45f404a06fec43b840bbc5cd0ec2250
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-07-19 15:31:38 -0500

    Update: 19/07/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-// src/App.jsx
 import React from 'react';
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Header from "./components/header.jsx";
 import AppRouter from './routers/AppRouter';
 import { ContactProvider } from './context/ContactContext';
+
+function AppContent() {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/notfound'; // ajusta esta condición según sea necesario
+
+  return (
+    <>
+      {showHeader && <Header />}
+      <AppRouter />
+    </>
+  );
+}
 
 function App() {
   return (
     <ContactProvider>
       <BrowserRouter>
-        <Header />
-        <AppRouter />
+        <AppContent />
       </BrowserRouter>
     </ContactProvider>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+// src/components/Header.jsx
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../assets/styles/header.css";
